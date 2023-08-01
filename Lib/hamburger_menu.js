de1bfa15536a9bdd5a0071aefc8819da953e64f1
Emtekz <reiterpaul02@gmail.com>
2023-08-01 10:21:50 +0200

    handy test

--- Lib/hamburger_menu.js
+++ Lib/hamburger_menu.js
@@ -1,7 +1,7 @@
 // 
 const menuIcon = document.querySelector('#menu');
 const navBar = document.querySelector('#navigation');
-const Header = document.querySelector('#header');
+const background = document.querySelector('#background-image');
 
 let open = false;
 
@@ -10,17 +10,25 @@ menuIcon.addEventListener('click', () => {
     
     if (!open) {
         menuIcon.src = "Images/menu_close.png";
-        Header.style.background = "white";
+        
         navBar.style.display = "flex";
+
+        background.style.filter = "brightness(35%)"
+        background.style.position = "fixed"
+
+        document.querySelector('body').style.overflow = "hidden";
         
         open = true;
     }else if (open) {
         menuIcon.src = "Images/menu_open.png";
-        Header.style.background = "none";
+        
         navBar.style.display = "none";
+        
+        background.style.filter = "brightness(65%)"
+        background.style.position = "";
+        
+        document.querySelector('body').style.overflow = "visible";
 
         open = false;
     }
-
-    // console.log('click');
 }) 
