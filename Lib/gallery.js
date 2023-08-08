310d4fad5c85170b48b3df2e75095314f7c7be06
Emtekz <reiterpaul02@gmail.com>
2023-08-08 09:12:11 +0200

    Erste "Sektionen"

--- Lib/gallery.js
+++ Lib/gallery.js
@@ -3,16 +3,22 @@ document.querySelectorAll('.image-container img').forEach(image => {
     image.onclick = () => {
         document.querySelector('.popup').style.display = 'block';
         document.querySelector('.popup img').src = image.getAttribute('src');
+
+        document.querySelector('body').style.overflow = "hidden";
     }
 })
 
 document.querySelector('.popup span').onclick = () => {
     document.querySelector('.popup').style.display = 'none';
+
+    document.querySelector('body').style.overflow = "visible";
 }
 
 document.onkeydown = function(evt) {
     evt = evt || window.event;
     if (evt.keyCode == 27) {
         document.querySelector('.popup').style.display = 'none';  
+        
+        document.querySelector('body').style.overflow = "visible";
     }
 };
